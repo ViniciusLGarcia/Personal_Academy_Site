d0065d3eebb5ce21fc2fcdd23b9855bfda292ef6
Thiago <thiagotenorio292@gmail.com>
2025-08-29 19:05:08 -0300

    Contact Us Section e Pré-Finalização.

--- Codigo/script.js
+++ Codigo/script.js
@@ -2,26 +2,42 @@
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn =  document.querySelector(".cancel-btn");
 let navBar = document.querySelector(".navbar");
+let body = document.querySelector("body");
+
 
 menuBtn.onclick = function() {
     menuBtn.style.opacity = "0";
     menuBtn.style.pointerEvents = "none";
     navBar.classList.add("active");
+    body.style.overflow = "hidden"
+    
 }  
 cancelBtn.onclick = function(){
     menuBtn.style.opacity = "1";
     menuBtn.style.pointerEvents = "auto";
     navBar.classList.remove("active");
+    body.style.overflow = "auto"
 
 }
 // Sticky Navigation Menu JS Code
 
-let nav = document.querySelector("nav");
 let val;
+let nav = document.querySelector("nav");
 window.onscroll = function(){
     if(document.documentElement.scrollTop > 20){
         nav.classList.add("sticky");
     }else{
         nav.classList.remove("sticky");
     }
+}
+//Side Navigation Menu CLosed When Navigation Links Clicked 
+let navLinks = document.querySelectorAll(".menu li a ");
+for (var i = 0; i < navLinks; i++){
+    navLinks[i].addEventListener("click", () => {
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    navBar.classList.remove("active");
+    body.style.overflow = "auto"
+
+    })
 }
